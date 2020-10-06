ecdf94c845033cd2ee778e0e49f7f2c8f4711187
Abhilash CR <abhilash.cr@mountblue.tech>
2020-10-06 19:33:24 +0530

    final commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,37 @@
+import Layout from "../../Components/Layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../Components/date";
+import Head from "next/head";
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        <Date dateString={postData.date} />
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
